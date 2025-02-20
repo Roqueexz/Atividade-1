b31f59fab2a98a1c5689b086b3ad76d9516274f9
Pedro Roque <roquefiesp@gmail.com>
2025-02-20 11:32:21 -0300

    Atividade 1

---
+++ variavel.js
@@ -0,0 +1,9 @@
+console.clear();
+var nome = "Roque";
+var idade = 15;
+var salve = "Ol\u00E1";
+var fut = "jogar futebol";
+var valorant = "jogar videogames";
+var mentira = "estudar";
+var facul = "engenharia de software";
+console.log("1- Na frase abaixo substitua Ol\u00E1 por uma vari\u00E1vel que vai carregar a informa\u00E7\u00E3o desejada.\n    \n".concat(salve, "!!\nMeu nome \u00E9 ").concat(nome, ", tenho ").concat(idade, " anos, no meu tempo livre gosto de ").concat(fut, ", ").concat(valorant, " e ").concat(mentira, ". \nTenho a inten\u00E7\u00E3o de me formar em ").concat(facul, "."));
---
+++ variavel.ts
@@ -0,0 +1,17 @@
+console.clear(); 
+let nome: string = `Roque`;
+let idade: number = 15;
+let salve: string = `Olá`
+let fut: string = `jogar futebol`
+let valorant: string = `jogar videogames`;
+let mentira: string = `estudar`;
+let facul: string = `engenharia de software`
+
+console.log(`1- Na frase abaixo substitua Olá por uma variável que vai carregar a informação desejada.
+    
+${salve}!!
+Meu nome é ${nome}, tenho ${idade} anos, no meu tempo livre gosto de ${fut}, ${valorant} e ${mentira}. 
+Tenho a intenção de me formar em ${facul}.`
+);
+
+
